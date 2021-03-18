9310394eb7b28258f0bf09b44c3a9b636c130ba5
djaciel <dj_roses@hotmail.com>
2021-03-17 23:36:55 -0600

    se crea llamada de playlists

--- blog/index.js
+++ blog/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const path = require("path");
-const request = require('request');
-const querystring = require('querystring');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const request = require("request");
+const querystring = require("querystring");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-const generateRandomString = require('./utils/generateRandomString');
-const encodeBasic = require('./utils/encodeBasic');
-const scopesArray = require('./utils/scopesArray');
+const generateRandomString = require("./utils/generateRandomString");
+const encodeBasic = require("./utils/encodeBasic");
+const scopesArray = require("./utils/scopesArray");
 
-const playlistMocks = require('./utils/mocks/playlist');
+const playlistMocks = require("./utils/mocks/playlist");
 
-const { config } = require('./config/index');
+const { config } = require("./config");
 
 const app = express();
 
@@ -26,14 +26,84 @@ app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+function getUserInfo(accessToken) {
+  if (!accessToken) {
+    return Promise.resolve(null);
+  }
+
+  const options = {
+    url: "https://api.spotify.com/v1/me",
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true
+  };
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        reject(error);
+      }
+
+      resolve(body);
+    });
+  });
+}
+
+function getUserPlaylists(accessToken, userId) {
+  if (!accessToken || !userId) {
+    return Promise.resolve(null);
+  }
+
+  const options = {
+    url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true
+  };
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        reject(error);
+      }
+
+      resolve(body);
+    });
+  });
+}
+
 // routes
 app.get("/", async function(req, res, next) {
-  res.render("playlists", 
-    { playlists: { items: playlistMocks } }
-  );
+  const { access_token: accessToken } = req.cookies;
+
+  try {
+    const userInfo = await getUserInfo(accessToken);
+    res.render("playlists", {
+      userInfo,
+      isHome: true,
+      playlists: { items: playlistMocks }
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
-app.get('/login', function(req, res) {
+app.get("/playlists", async function(req, res, next) {
+  const { access_token: accessToken } = req.cookies;
+
+  if (!accessToken) {
+    return res.redirect("/");
+  }
+
+  try {
+    const userInfo = await getUserInfo(accessToken);
+    const userPlaylists = await getUserPlaylists(accessToken, userInfo.id);
+
+    res.render("playlists", { userInfo, playlists: userPlaylists });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/login", function(req, res) {
   const state = generateRandomString(16);
 
   const queryString = querystring.stringify({
@@ -46,9 +116,14 @@ app.get('/login', function(req, res) {
 
   res.cookie("auth_state", state, { httpOnly: true });
   res.redirect(`https://accounts.spotify.com/authorize?${queryString}`);
-})
+});
 
-app.get('/callback', function(req, res, next) {
+app.get("/logout", function(req, res) {
+  res.clearCookie("access_token");
+  res.redirect("/");
+});
+
+app.get("/callback", function(req, res, next) {
   const { code, state } = req.query;
   const { auth_state } = req.cookies;
 
@@ -69,9 +144,9 @@ app.get('/callback', function(req, res, next) {
       Authorization: `Basic ${encodeBasic(
         config.spotifyClientId,
         config.spotifyClientSecret
-      )}`,
-      json: true
-    }
+      )}`
+    },
+    json: true
   };
 
   request.post(authOptions, function(error, response, body) {
@@ -81,7 +156,7 @@ app.get('/callback', function(req, res, next) {
 
     res.cookie("access_token", body.access_token, { httpOnly: true });
     res.redirect("/playlist");
-  })
+  });
 });
 
 // server
